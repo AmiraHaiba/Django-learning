4f76328804938d382dc5f5b4da3e5451a1d9642f
yasser-albarbary <Yasser.Al-Barbary@acutest.com>
2020-06-22 00:15:47 +0100

    added more features

--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-posts = [
-    {
-    'author': 'Amira',
-    'date': '20/2/2020',
-    'content': 'I am learinig how to use django framework'
-    },
-    {
-    'author': 'Yasser',
-    'date': '20/2/2020',
-    'content': 'I am learinig how to use django framework'
-    }
-]
-# Create your views here.
+from .models import Post
+
 def home(request):
     context = {
-    'posts': posts
+    'posts': Post.objects.all()
     }
     return render(request, 'posts/home.html', context)
 
